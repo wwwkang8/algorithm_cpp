827c3deac23bf2094ef2ab4d48f889bbca334490
JeongHo <wwwkang8@gmail.com>
2018-09-27 20:49:28 +0900

    알파벳 찾기

---
+++ alphabet/alphabet.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	string str;
+	cin >> str;
+
+	int idx;
+	for (char a = 'a'; a <= 'z'; a++) {
+		idx = str.find(a);
+		cout << idx << " ";
+	}
+
+	cout << '\n';
+
+
+
+	return 0;
+}
