28c3d2e4dc956de1dd759e6dd0cdf82dabb52f4c
wwwkang8 <wwwkang8@gmail.com>
2022-03-01 23:51:22 +0900

    3-2. 큰수의 법칙

---
+++ 3. 이것이코딩테스트다/3-2. 큰수의 법칙/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<queue>
+#include<map>
+#include <sstream>
+using namespace std;
+
+
+
+int main(void){
+
+
+
+
+    return 0;
+}
