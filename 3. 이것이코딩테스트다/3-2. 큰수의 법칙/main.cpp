2f9123f8f3071537da6c4604a984a52de9d52930
wwwkang8 <wwwkang8@gmail.com>
2022-03-16 00:52:48 +0900

    큰수의 법칙

--- 3. 이것이코딩테스트다/3-2. 큰수의 법칙/main.cpp
+++ 3. 이것이코딩테스트다/3-2. 큰수의 법칙/main.cpp
@@ -7,9 +7,51 @@
 #include <sstream>
 using namespace std;
 
+//a와 b를 비교하여, a>b의 결과를 반환한다.
+bool desc(int a, int b){
+    return a > b;
+}
 
 
 int main(void){
+
+    // N : 배열의 크기, M : 숫자가 더해지는 횟수, K : 연속해서 더해질 수 있는 최대 횟수
+    int N, M, K;
+    int answer = 0;
+
+    cin >> N >> M >> K;
+
+    int array[N];
+
+    for(int i=0; i<N; i++){
+        cin >> array[i];
+    }
+
+    sort(array, array+5, desc);
+
+    // 1. 배열 내에서 첫번쨰로 큰수, 두번쨰로 큰수를 찾아서 더하기만 하면된다.
+    while(true) {
+
+        for(int i=0; i<K; i++) {
+            if(M == 0) {
+                break;
+            }
+
+            answer += array[0];
+            M--;
+
+        }
+
+        if(M == 0) {
+            break;
+        }
+
+        answer += array[1];
+        M--;
+
+    }
+
+    cout << answer;
 
 
 
